566acce44a7599cc13fdf0f07fbcf1c8c2015abd
Julian Hernandez <gmfk07@gmail.com>
2017-09-16 15:44:07 -0400

    The Intro
    Intro done

--- init.py
+++ init.py
@@ -4,32 +4,48 @@ Created on Sat Sep 16 12:04:14 2017
 
 @author: gmfk07
 """
-import random
+import random, intro
 #Randomize values
 random.seed()
 
-class victim(Object):
-    def __init__(self, name, trait1, trait2):
+class victim():
+    def __init__(self, name, trait1, trait2, age):
         self.name = name
         self.trait1 = trait1
         self.trait2 = trait2
+        self.age = age
+        
+    def getName(self):
+        return self.name
+    
+    def getTrait1(self):
+        return self.trait1
+    
+    def getTrait2(self):
+        return self.trait2
+    
+    def getAge(self):
+        return self.age
 
 #Day
-month = random.choice("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+mts = "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+month = random.choice(mts)
 day = random.randint(1,28)
 #MurderTime
-murderHour = random.choice(11,12,1,2,3,4)
-murderMinute = random.randint(0,59)
+murderHour = random.choice((11,12,1,2,3,4))
 if murderHour > 10:
     murderTOD = "pm"
 else:
     murderTOD = "am"
 #Murder Details
-murderWeapon = random.choice("gun","knife","poison","club","strangling")
-t1 = random.choice("rich", "politician", "average")
-t2 = random.choice("friend", "boss child", "good")
-murderVictim = victim("Andrew Hoyt", "rich", "friend")
-murderLocation = ("alleyway", "warehouse", "hotel room", "house")
+murderWeapon = random.choice(("gun","knife","poison","club","strangling"))
+t1 = random.choice(("rich", "politician", "average"))
+t2 = random.choice(("friend", "boss child", "good"))
+age = random.randint(23,42)
+murderVictim = victim("Andrew Hoyt", t1, t2, 69)
+murderLocation = random.choice(("alleyway", "warehouse", "hotel room", "house"))
 #Complications/plot twists
 complicationCount = 2;
 complications = ("wrong weapon", "wrong location", "wrong identity", "missing evidence", "planted evidence", "suspect gone")
+
+intro.intro(murderVictim, month, day, murderWeapon, murderLocation, murderHour, murderTOD)
---
+++ intro.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 14:20:46 2017
+
+@author: gmfk07
+"""
+import random
+
+def intro(victim, month, day, murderWeapon, murderLocation, murderHour, murderTOD):
+    name = victim.getName()
+    t1 = victim.getTrait1()
+    t2 = victim.getTrait2()
+    age = victim.getAge()
+    
+    if t1 == "rich":
+        t1line = "rather wealthy."
+    if t1 == "politician":
+        t1line = "climbing up the ranks to be a successful politician."
+    if t1 == "average":
+        t1line = "worked a decent-paying job."
+    
+    if t2 == "friend":
+        t2line = "You remember them fondly - they were your best friend back in college."
+    if t2 == "boss child":
+        t2line = "The boss handed you the case personally, making you promise to catch the culprit who killed their " + random.choice(("only child.","eldest child."))
+    if t2 == "good":
+        t2line = "It seems the whole city is grieving over the sudden loss of someone that special."
+ 
+    if murderWeapon == "gun":
+        mword = "shot"
+    if murderWeapon == "knife":
+        mword = "stabbed"
+    if murderWeapon == "poison":
+        mword = "poisoned"
+    if murderWeapon == "club":
+        mword = "bludgeoned"
+    if murderWeapon == "strangling":
+        mword = "strangled"
+        
+    if murderLocation == "alleyway":
+        mline = "some old alleyway that nobody bothered to renovate."
+    if murderLocation == "warehouse":
+        mline = "a warehouse down by the docks."
+    if murderLocation == "hotel room":
+        mline = "a hotel room on the nicer side of town."
+    if murderLocation == "house":
+        mline = "the victim’s own house."
+    
+    print ("This city is " + random.choice(("crime-ridden", "dark")) + " and " + random.choice(("brutal", "seedy")) + " and you " + random.choice(("love it", "hate it", "got used to it")) + ".")
+    print ("You got your most recent case handed to you unceremoniously - solving the murder of " + name + ". " + t2line)
+    print ("On " + month + " " + str(day) + " at " + str(murderHour) + " " + murderTOD + ", " + name + " was murdered, and the autopsy report suggests they were " + mword + ". At the time of their death, they were " + str(age) + " and " + t1line + " The crime scene is " + mline)
+    print ("Nothing left to do now but solve the case and then " + random.choice(("go on that vacation you’ve always wanted", "retire", "go back to work next week")) + ".")
